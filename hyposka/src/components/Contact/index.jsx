96b4f31b31cd8146abcc725d9a3a500e9f26ac5a
Veronika Sípalová <ver.kosik@gmail.com>
2021-05-23 12:17:23 +0200

    přidané kontakty

--- hyposka/src/components/Contact/index.jsx
+++ hyposka/src/components/Contact/index.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
+import email from './img/email.svg';
+import github from './img/logo-git.svg';
+
 export const Contact = () => {
-  return <h1>Kontakty</h1>;
+  return (
+    <>
+      <header>
+        <h1>Kontakty</h1>;
+      </header>
+      <p>Máš pro nás radu? Napiš, budeme rády!</p>
+      <h2>Slávka Vavřinová</h2>
+      <div className="contact">
+        <img className={email} src="img/email.svg" alt="E-mail" />
+        <p>slavka.vav@gmail.com</p>
+      </div>
+      <div className="contact">
+        <img className={github} src="img/logo-git.svg" alt="Logo GitHub" />
+        <p>SlavkaVavrinova</p>
+      </div>
+      <h2>Veronika Sípalová</h2>
+      <div className="contact">
+        <img className={email} src="img/email.svg" alt="E-mail" />
+        <p>ver.kosik@gmail.com</p>
+      </div>
+      <div class="contact">
+        <img className={github} src="img/logo-git.svg" alt="Logo GitHub" />
+        <p>Veronika Sípalová</p>
+      </div>
+    </>
+  );
 };
