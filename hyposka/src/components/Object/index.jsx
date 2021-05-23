3d9f84d39435dc85cfdcae59d36a8357e4ca9f49
SlavkaVavrinova <slavka.vav@gmail.com>
2021-05-23 11:24:35 +0200

    Navigace

---
+++ hyposka/src/components/Object/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const Object = () => {
+  return (
+    <>
+      <h1>Účely</h1>
+      <p>Jaký je tvůj plán? Co zaplatíš hypotékou? Vyber si:</p>
+    </>
+  );
+};
