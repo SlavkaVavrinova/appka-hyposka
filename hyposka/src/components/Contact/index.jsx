1e3b194ee5f21c52f7f798206ea130375efe7941
Veronika Sípalová <ver.kosik@gmail.com>
2021-05-23 12:46:53 +0200

    úprava kontaktů

--- hyposka/src/components/Contact/index.jsx
+++ hyposka/src/components/Contact/index.jsx
@@ -7,26 +7,26 @@ export const Contact = () => {
   return (
     <>
       <header>
-        <h1>Kontakty</h1>;
+        <h1>Kontakty</h1>
       </header>
       <p>Máš pro nás radu? Napiš, budeme rády!</p>
       <h2>Slávka Vavřinová</h2>
       <div className="contact">
-        <img className={email} src="img/email.svg" alt="E-mail" />
+        <img className="email" src={email} alt="E-mail" />
         <p>slavka.vav@gmail.com</p>
       </div>
       <div className="contact">
-        <img className={github} src="img/logo-git.svg" alt="Logo GitHub" />
+        <img className="github" src={github} alt="Logo GitHub" />
         <p>SlavkaVavrinova</p>
         <button>Více o Slávce</button>
       </div>
       <h2>Veronika Sípalová</h2>
       <div className="contact">
-        <img className={email} src="img/email.svg" alt="E-mail" />
+        <img className="email" src={email} alt="E-mail" />
         <p>ver.kosik@gmail.com</p>
       </div>
-      <div class="contact">
-        <img className={github} src="img/logo-git.svg" alt="Logo GitHub" />
+      <div className="contact">
+        <img className="github" src={github} alt="Logo GitHub" />
         <p>Veronika Sípalová</p>
         <button>Více o Verče</button>
       </div>
