60657fe3eed87b675fe3c7a4f8e435e695e5f61b
SlavkaVavrinova <slavka.vav@gmail.com>
2021-05-23 15:40:59 +0200

    úpravy

--- hyposka/src/components/Mortgage/index.jsx
+++ hyposka/src/components/Mortgage/index.jsx
@@ -30,23 +30,3 @@ export const Mortgage = () => {
     </>
   );
 };
-
-/*
-{purposes.map((purpose) => (
-        <Card {...purpose} key={purpose.title} />
-      ))}*/
-
-const purposes = [
-  {
-    photo: './img/buy-house.jpg',
-    alt: 'Rodinný dům',
-    title: 'Koupit dům',
-    text: 'Chci koupit rodinný dům, kde plánuji bydlet. Pouze koupě bez rekonstrukce',
-  },
-  {
-    photo: './img/land.jpg',
-    alt: 'Stavební pozemek',
-    title: 'Koupit stavební pozemek',
-    text: 'Chci koupit pozemek, kde budu stavět, ale zatím ho jen kupuji.',
-  },
-];
