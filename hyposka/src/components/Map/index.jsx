461be04d03034c418ead3e507ee229600e81ff0b
SlavkaVavrinova <slavka.vav@gmail.com>
2021-05-27 20:45:14 +0200

    Změny v mapě

--- hyposka/src/components/Map/index.jsx
+++ hyposka/src/components/Map/index.jsx
@@ -12,15 +12,12 @@ export const Map = () => {
         Neboj žádné vyplňte formulář a kontaktuje Vás finační poradce tu není!
       </p>
       <p>
-        Toto je mapa<em> "udělej si sám"</em>
-      </p>
-      <p>
-        V mapě najdeš pracovníky z bank a finanční poradce, kteří umí hypotéky a
-        jejich klienti jsou s nimi pokojeni. Jak jsme to zjistili? Pod mapou
-        najdeš formulář, který můžeš vyplnit i ty. Máš v okolí někoho o kom víš,
-        že hypotéky zpracovává a má spokojené kliety? Nebo ti někdo takový
-        hypotéku zařizoval? Budeme velice rádi, když referenci na něj dodáš do
-        naší mapy.
+        Toto je mapa<em> "udělej si sám"</em>. V mapě najdeš pracovníky z bank a
+        finanční poradce, kteří umí hypotéky a jejich klienti jsou s nimi
+        pokojeni. Jak jsme to zjistili? Pod mapou najdeš formulář, který můžeš
+        vyplnit i ty. Máš v okolí někoho o kom víš, že hypotéky zpracovává a má
+        spokojené kliety? Nebo ti někdo takový hypotéku zařizoval? Budeme velice
+        rádi, když referenci na něj dodáš do naší mapy.
       </p>
       <p>Jak to funguje?</p>
       <p>
--- hyposka/src/components/MapBox/index.jsx
+++ hyposka/src/components/MapBox/index.jsx
@@ -8,7 +8,7 @@ export const MapBox = () => {
   const [viewport, setViewport] = useState({
     latitude: 48.9747357,
     longitude: 14.474285,
-    zoom: 15,
+    zoom: 7,
   });
 
   const bodZajmu = {
@@ -17,12 +17,13 @@ export const MapBox = () => {
     longitude: 14.473364057153596,
   };
 
-  const restaurace = [
+  const persons = [
     {
       id: 1,
       nazev: 'Eva ',
       latitude: 48.974598,
       longitude: 14.479637,
+      typ: 'Bankéř/ka',
       ikonka: bankPoint,
     },
     {
@@ -30,6 +31,7 @@ export const MapBox = () => {
       nazev: 'Šárka',
       latitude: 48.815727,
       longitude: 14.314896,
+      typ: 'Bankéř/ka',
       ikonka: bankPoint,
     },
     {
@@ -37,6 +39,7 @@ export const MapBox = () => {
       nazev: 'Léni paní',
       latitude: 49.316744,
       longitude: 14.245258,
+      typ: 'Finanční poradce/poradkyně',
       ikonka: consultantPoint,
     },
     {
@@ -44,57 +47,97 @@ export const MapBox = () => {
       nazev: 'CK',
       latitude: 48.8193497,
       longitude: 14.31946,
+      typ: 'Finanční poradce/poradkyně',
       ikonka: consultantPoint,
     },
   ];
+  const [otevrenyPopup, setOtevrenyPopup] = useState(null);
+
+  const [filtr, setFiltr] = useState('Vše');
 
   return (
-    <ReactMapGL
-      {...viewport}
-      onViewportChange={(novyViewport) => setViewport(novyViewport)}
-      width="100%"
-      height={400}
-      mapStyle={{
-        version: 8,
-        sources: {
-          'raster-tiles': {
-            type: 'raster',
-            tiles: ['https://mapserver.mapy.cz/base-m/{z}-{x}-{y}'],
-            tileSize: 256,
-            attribution:
-              'Mapové podklady od <a target="_top" rel="noopener" href="https://mapy.cz/">Seznam.cz</a> a <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>.',
+    <>
+      <ReactMapGL
+        {...viewport}
+        onViewportChange={(novyViewport) => setViewport(novyViewport)}
+        width="100%"
+        height={400}
+        mapStyle={{
+          version: 8,
+          sources: {
+            'raster-tiles': {
+              type: 'raster',
+              tiles: ['https://mapserver.mapy.cz/base-m/{z}-{x}-{y}'],
+              tileSize: 256,
+              attribution:
+                'Mapové podklady od <a target="_top" rel="noopener" href="https://mapy.cz/">Seznam.cz</a> a <a target="_top" rel="noopener" href="http://openstreetmap.org">OpenStreetMap</a>.',
+            },
           },
-        },
-        layers: [
-          {
-            id: 'simple-tiles',
-            type: 'raster',
-            source: 'raster-tiles',
-            minzoom: 0,
-            maxzoom: 20,
-          },
-        ],
-      }}
-    >
-      {restaurace.map((person) => (
-        <React.Fragment key={person.id}>
-          <Marker
-            latitude={person.latitude}
-            longitude={person.longitude}
-            offsetLeft={-25}
-            offsetTop={-50}
-          >
-            <img src={person.ikonka} width={50} height={50} alt="" />
-          </Marker>
-          <Popup
-            offsetTop={-60}
-            latitude={person.latitude}
-            longitude={person.longitude}
-          >
-            {person.nazev}
-          </Popup>
-        </React.Fragment>
-      ))}
-    </ReactMapGL>
+          layers: [
+            {
+              id: 'simple-tiles',
+              type: 'raster',
+              source: 'raster-tiles',
+              minzoom: 0,
+              maxzoom: 20,
+            },
+          ],
+        }}
+      >
+        {persons
+          .filter((person) => {
+            if (filtr === 'Vše') {
+              return true;
+            }
+            if (filtr === person.typ) {
+              return true;
+            }
+            return false;
+          })
+          .map((person) => (
+            <React.Fragment key={person.id}>
+              <Marker
+                latitude={person.latitude}
+                longitude={person.longitude}
+                offsetLeft={-25}
+                offsetTop={-50}
+              >
+                <div
+                  onClick={() => {
+                    setOtevrenyPopup(person.id);
+                  }}
+                  className="mapa__marker"
+                >
+                  <img src={person.ikonka} width={50} height={50} alt="" />
+                </div>
+              </Marker>
+              {otevrenyPopup === person.id ? (
+                <Popup
+                  offsetTop={-60}
+                  latitude={person.latitude}
+                  longitude={person.longitude}
+                  onClose={() => {
+                    setOtevrenyPopup(null);
+                  }}
+                >
+                  <h2>{person.nazev}</h2>
+                  <p>{person.typ}</p>
+                </Popup>
+              ) : null}
+            </React.Fragment>
+          ))}
+      </ReactMapGL>
+      <label htmlFor="">
+        Zobrazit v mapě:
+        <select
+          value={filtr}
+          onChange={(event) => setFiltr(event.target.value)}
+        >
+          <option>Vše</option>
+          <option>Bankéř/ka</option>
+          <option>Finanční poradce/poradkyně</option>
+        </select>
+      </label>
+    </>
   );
 };
