214e1c5a26c5edfb02eab91b50a1420d0bd71eba
Veronika Sípalová <ver.kosik@gmail.com>
2021-05-30 15:31:01 +0200

    proklik na další otázku

--- hyposka/src/components/Questions/questions.jsx
+++ hyposka/src/components/Questions/questions.jsx
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import cx from 'classnames/bind';
 import './style.css';
 
 export const Questions = (props) => {
+  const handleRightAnswer = (event) => {
+    props.goAhead();
+  };
+  const handleWrongAnswer = (event) => {
+    alert('Zkus to znovu');
+  };
+
   return (
     <>
-      <div className="id">
+      <div className={cx('id', props.className)}>
         <span className={props.key}></span>
         <span className="question">{props.title}</span>
-        <span className="answer">{props.textA}</span>
-
-        <span className="answer">{props.textB}</span>
+        <span className="answer" onClick={handleRightAnswer}>
+          {props.textA}
+        </span>
+        <span className="answer" onClick={handleWrongAnswer}>
+          {props.textB}
+        </span>
       </div>
-
       <img className="answer__img" src={props.photo} />
     </>
   );
--- hyposka/src/components/Quiz/index.jsx
+++ hyposka/src/components/Quiz/index.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import cx from 'classnames/bind';
 import { Questions } from '../Questions/questions.jsx';
 import './style.css';
 import Castle from './img/castle.svg';
@@ -8,6 +9,7 @@ import Flat from './img/flat.svg';
 import House from './img/house.svg';
 
 export const Quiz = () => {
+  const [activeQuestion, setActiveQuestion] = useState(1);
   return (
     <>
       <main>
@@ -16,41 +18,50 @@ export const Quiz = () => {
           Vyzkoušej své znalosti. Každá správná odpověď tě posune o krok blíž k
           lepšímu bydlení.
         </h2>
-
         <Questions
+          className={cx({ 'is-active': activeQuestion === 1 })}
           key="Question1"
           title="Co je to hypoška?"
-          textA="A. Hypoteční úvěr na bydlení."
-          textB="B. Úvodní fáze při hypnóze."
+          textA="Hypoteční úvěr na bydlení."
+          textB="Úvodní fáze při hypnóze."
           photo={Ruin}
+          goAhead={() => setActiveQuestion(activeQuestion + 1)}
         ></Questions>
         <Questions
+          className={cx({ 'is-active': activeQuestion === 2 })}
           key="Question2"
           photo={Tent}
           title="Co je to fixace?"
-          textA="A. Doba, pro kterou bude platit sjednaný úrok."
-          textB="B. Když muž potká ženu, tak mezi nimi vznikne fixace."
+          textA="Doba, pro kterou bude platit sjednaný úrok."
+          textB="Když muž potká ženu, tak mezi nimi vznikne fixace."
+          goAhead={() => setActiveQuestion(activeQuestion + 1)}
         ></Questions>
         <Questions
+          className={cx({ 'is-active': activeQuestion === 3 })}
           key="Question3"
           photo={Flat}
           title="Jaká fixace úroku je nejlepší?"
-          textA="A. Variabilní."
-          textB="B. Čím delší, tím lepší."
+          textA="Variabilní."
+          textB="Čím delší, tím lepší."
+          goAhead={() => setActiveQuestion(activeQuestion + 1)}
         ></Questions>
         <Questions
+          className={cx({ 'is-active': activeQuestion === 4 })}
           key="Question4"
           photo={House}
           title="Kde najdeš nejlevnější hypotéku?"
-          textA="A. Každá banka má v různý moment jinou sazbu. Je tedy buď na tobě si banky obejít nebo si sehnat nějakého finančního poradce"
-          textB="B. Zeptám se souseda či kamaráda."
+          textA="Každá banka má v různý moment jinou sazbu. Je tedy buď na tobě si banky obejít nebo si sehnat nějakého finančního poradce"
+          textB="Zeptám se souseda či kamaráda."
+          goAhead={() => setActiveQuestion(activeQuestion + 1)}
         ></Questions>
         <Questions
+          className={cx({ 'is-active': activeQuestion === 5 })}
           key="Question5"
           photo={Castle}
           title="Co je zástavní smlouva?"
-          textA="A. Smlouva, která se vystaví po podpisu smlouvy o hypotéce."
-          textB="B. Když zastavím na zákazu zastavení, tak mi ji vystaví policie ČR, aby mi pomohla a ochránila mě."
+          textA="Smlouva, která se vystaví po podpisu smlouvy o hypotéce."
+          textB="Když zastavím na zákazu zastavení, tak mi ji vystaví policie ČR, aby mi pomohla a ochránila mě."
+          goAhead={() => setActiveQuestion(activeQuestion + 1)}
         ></Questions>
       </main>
     </>
