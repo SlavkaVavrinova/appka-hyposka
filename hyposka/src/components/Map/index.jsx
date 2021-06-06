c2a10901d127731680d4dec1e455cda4eea6872f
Veronika Sípalová <ver.kosik@gmail.com>
2021-06-06 17:01:32 +0200

    text u mapy

--- hyposka/src/components/Map/index.jsx
+++ hyposka/src/components/Map/index.jsx
@@ -10,13 +10,14 @@ export const Map = () => {
       <main>
         <h1>Mapa poradců</h1>
         <p>
-          Neboj žádné vyplňte formulář a kontaktuje Vás finační poradce tu není!
+          Neboj, žádné "Vyplňte formulář" a kontaktuje Vás finační poradce tu
+          není!
         </p>
         <p>
           Toto je mapa<em> "udělej si sám"</em>. V mapě najdeš pracovníky bank a
           finanční poradce, kteří umí hypotéky a jejich klienti jsou s nimi
           pokojeni. Jak jsme to zjistili? Pod mapou najdeš formulář, který můžeš
-          vyplnit i ty. Máš v okolí někoho o kom víš, že hypotéky zpracovává a
+          vyplnit i ty. Máš v okolí někoho, o kom víš, že hypotéky zpracovává a
           má spokojené kliety? Nebo ti někdo takový hypotéku zařizoval? Budeme
           velice rádi, když referenci na něj přidáš do naší mapy.
         </p>
@@ -26,7 +27,7 @@ export const Map = () => {
           poradce kontaktujeme a s jeho souhlasem zveřejníme.{' '}
         </p>
         <p>
-          Z toho plyne, prosím pouze o pozitivní zkušenost. Jistě se najdou i
+          Z toho plyne, prosím, pouze o pozitivní zkušenost. Jistě se najdou i
           hororové příběhy, ale tady najdeš jen "top" poradce. Chceme se vyhnout
           změně na skládku těch špatných.
         </p>
@@ -51,7 +52,7 @@ export const Map = () => {
         )}
 
         <h3>Zadej poradce</h3>
-        <p>Jsi spokojen se zpracovatelem hypotéky. Zadej svou zkušenost</p>
+        <p>Jsi spokojen se zpracovatelem hypotéky. Zadej svou zkušenost.</p>
         <FormMap />
       </main>
     </>
